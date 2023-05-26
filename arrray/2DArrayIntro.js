dd7dc2401b19af2bdf45c3053b6c96c1047626a9
iamdeepakram <deepakramsutrakar@gmail.com>
2023-05-26 16:16:43 +0530

    added intro to 2D array

---
+++ arrray/2DArrayIntro.js
@@ -0,0 +1,36 @@
+
+let arr = [[1,2,3],[4,5,6], [7,8,9]];
+
+
+// pop()
+
+// push()
+
+// update --> Replace 3 to 10
+// arr[0][2] = 10;
+
+// create
+
+// Read 5
+console.log(arr[1][1]);
+// delete
+
+
+// Iterate over array
+
+for(let i=0; i<arr.length; i++){
+    console.log(arr[i]);
+}
+
+// Iterate over sub array
+
+// for(let i =0; i<arr.length; i++){
+//     let subArray = arr[i];
+//     for(let n=0; n<subArray.length; n++){
+//         console.log(subArray[n]);
+//     }
+// }
+
+
+const dv = 'something';
+console.log(dv);
